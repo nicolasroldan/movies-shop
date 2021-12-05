d31f6675428283563e6b54d4c6ff27d6c11b158f
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-05 17:03:18 -0300

    Created login form.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  @Output() toggleRegisterSection: EventEmitter<void> = new EventEmitter();
+  public loginForm: FormGroup;
+  @ViewChild('passwordInput', { static: true }) passwordInput: ElementRef;
+  public showPassword: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private moviesService: MoviesService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
   }
 
-  onToggleRegisterSection(): void {
-    this.toggleRegisterSection.emit();
+  get email(): AbstractControl | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['register']);
+  }
+
+  onTogglePassword(): void {
+    this.showPassword = !this.showPassword;
+    this.passwordInput.nativeElement.type = this.passwordInput.nativeElement.type === 'password' ? 'text' : 'password';
+  }
+
+  onSubmit(): void {
+    this.router.navigate(['dashboard']);
   }
 }
