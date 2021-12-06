d65bd469a6283062f7741960ab7493aaac2a130f
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-06 14:57:44 -0300

    Added new route for movie details view.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { MovieDetailsComponent } from './components/movies-grid/movie-details/movie-details.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'movie/2', component: MovieDetailsComponent }
 ];
 
 @NgModule({
