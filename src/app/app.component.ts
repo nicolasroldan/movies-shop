5cb0a56c6f015909a9655bca9eb4318d236c3722
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-06 14:43:49 -0300

    Added footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies-shop';
+
+  constructor(private router: Router) {
+
+  }
+
+  hasRoute(routes: string[]): boolean {
+   return !!routes.find(route => route === this.router.url);
+  }
 }
