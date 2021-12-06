bc0f7f09d23765f702975a632b6a12bbfb07f997
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-06 16:49:52 -0300

    Header modifications.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,4 +14,8 @@ export class HeaderComponent implements OnInit {
   goToCart(): void {
     this.router.navigate(['cart']);
   }
+
+  goToDashboard(): void {
+    this.router.navigate(['dashboard']);
+  }
 }
