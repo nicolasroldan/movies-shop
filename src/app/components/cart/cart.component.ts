b876287a4524e064f5dfd9ae159a4085f956fcf6
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-07 19:10:15 -0300

    Implemented real data in cart component.

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  public user: User;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.user = this.userService.getUser();
   }
 
+  calculateTotalPrice(): number {
+    return this.user.moviesInCart.map(movie => movie.price).reduce((a, b) => { return a + b }, 0);
+  }
 }
