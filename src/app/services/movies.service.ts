4466ac7d45781d6bef1d9170e1adee45248a345d
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-07 19:05:16 -0300

    Changed movies service to implement httpClient.

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { MOVIES } from '../mocks/mock-movies';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Movie } from '../models/Movie';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-  constructor() {}
+  constructor(private http: HttpClient) { }
 
   getMovies(): Observable<Movie[]> {
-    const movies = of(MOVIES);
-    return movies;
+    return this.http.get<Movie[]>('../../assets/mocks/movies-list-mock.json');
+  }
+
+  getMovie(movieId: string): Observable<Movie[]> {
+    return this.http.get<Movie[]>('../../assets/mocks/movies-list-mock.json')
+      .pipe(
+        map(response => response.filter((data) => data.id === parseInt(movieId)))
+      )
   }
 }
