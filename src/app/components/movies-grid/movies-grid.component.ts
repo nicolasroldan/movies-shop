2b39f9acbe356b502ef3fcbfbb929532b61aeef6
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-07 19:14:39 -0300

    Implemented real data in movies grid component.

--- src/app/components/movies-grid/movies-grid.component.ts
+++ src/app/components/movies-grid/movies-grid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Movie } from 'src/app/models/Movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movies-grid',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-grid.component.scss']
 })
 export class MoviesGridComponent implements OnInit {
-
-  constructor() { }
+  public movies: Movie[] = [];
+  constructor(private moviesService: MoviesService, private router: Router) { }
 
   ngOnInit(): void {
+    this.moviesService.getMovies().subscribe(movies => {
+      this.movies = movies;
+    })
+
   }
 
+  goToMovieDetails(movie: Movie): void {
+    this.router.navigate([`/movies/details/${movie.id}`]);
+  }
+
+  goToMoviesList(): void {
+    document.getElementById('movies')?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+      });
+  }
 }
