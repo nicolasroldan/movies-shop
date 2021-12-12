aa8e23a0fe03bd74ad48883fbf5fcb0f576becef
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-12 15:25:30 -0300

    Implemented validation in register view.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,18 +1,22 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   public registerForm: FormGroup;
   @ViewChild('passwordInput', { static: true }) passwordInput: ElementRef;
   public showPassword: boolean = false;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor(private router: Router, private fb: FormBuilder) {}
+  constructor(private router: Router, private fb: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -37,14 +41,32 @@ export class RegisterComponent implements OnInit {
     return this.registerForm.get('password');
   }
 
-  goToLogin(): void {
+  public goToLogin(): void {
     this.router.navigate(['login']);
   }
 
-  onTogglePassword(): void {
+  public onTogglePassword(): void {
     this.showPassword = !this.showPassword;
     this.passwordInput.nativeElement.type = this.passwordInput.nativeElement.type === 'password' ? 'text' : 'password';
   }
 
-  onSubmit(): void {}
+  public onSubmit(): void {
+    if (this.name && this.email && this.password) {
+      const user: User = {
+        name: this.name.value,
+        email: this.email.value,
+        password: this.password.value,
+        isAdmin: false,
+        moviesInCart: [],
+        rentedMovies: []
+      }
+      this.subscriptions.add(
+        this.userService.createUser(user).subscribe(() => this.router.navigate(['login']))
+      );
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
