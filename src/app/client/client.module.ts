828f1d5925ea776f71a0f4749ec4073b5b3a6e48
Nicolas Roldan <nicolasroldan1994@gmail.com>
2021-12-15 20:37:38 -0300

    Created components folder in Client module.

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ClientRoutingModule } from './client-routing.module';
 
-import { MoviesGridComponent } from './movies-grid/movies-grid.component';
-import { CartComponent } from './cart/cart.component';
-import { MovieDetailsComponent } from './movies-grid/movie-details/movie-details.component';
+import { MoviesGridComponent } from './components/movies-grid/movies-grid.component';
+import { CartComponent } from './components/cart/cart.component';
+import { MovieDetailsComponent } from './components/movies-grid/movie-details/movie-details.component';
 
 
 @NgModule({
